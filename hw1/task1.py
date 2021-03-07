50c57f6e3339e576a802c689ef1204f585bf4ed2
Adelya <65678470+Adelechka@users.noreply.github.com>
2021-03-07 14:33:55 +0300

    homework2

--- hw1/task1.py
+++ hw1/task1.py
@@ -1,1 +1,14 @@
-print("hello world")
+import vk_api;
+import requests;
+
+token = '171b8e8d7ebbfd17ea07041e8891613e257efe8185c70f520ab1e8b3236c8fec81fed32e77f13569c6722';
+version = 5.92
+domain = 'khm'
+
+responce = requests.get('https://api.vk.com/method/groups.get',
+                        params={
+                            'access_token': token,
+                            'v': version,
+                            'domain': domain
+                        })
+print(responce.json())
